e9687ac99341796d6b0d5bd6ee1e71ad9994b9fb
kimmie-78 <101290243+kimmie-78@users.noreply.github.com>
2023-05-01 17:51:20 +0200

    first commit

--- script.js
+++ script.js
@@ -4,3 +4,35 @@ const signUpBtn = document.querySelector(".green-bg button");
 signUpBtn.addEventListener("click", () => {
   container.classList.toggle("change");
 });
+
+//collecting data fromtextboxes
+function test(){
+
+//retrieving the data
+ var Uname=document.getElementById('Uname').value
+ console.log(Uname);
+
+ var Fname=document.getElementById('Fname').value
+ console.log(Fname);
+
+ const Calder = document.querySelector('input[type="date"]');
+ console.log(Calder.value);
+
+ let Cnum=document.getElementById('Cnum').value
+ console.log(Cnum);
+
+ let emil=document.getElementById('emil').value
+ console.log(emil);
+
+ let pass=document.getElementById('pass').value
+ console.log(pass);
+ //storing the data
+ var user =localStorage.setItem("Uname", Uname);
+ var fuser =localStorage.setItem("Fname", Fname);
+ var Calde =localStorage.setItem("Calder", Calder);
+ var cnum =localStorage.setItem("  Cnum", Cnum);
+ var emill =localStorage.setItem("emil", emil);
+ var passw=localStorage.setItem("pass", pass);
+
+
+}
